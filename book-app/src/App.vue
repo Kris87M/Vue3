5f9c573d9f7720c460621421b16674d5a3f01b2f
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-11 17:28:39 +0100

    Init book-app project

---
+++ book-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+
+    <!-- heading -->
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+
+    <!-- books list -->
+    <ul>
+    </ul>
+
+    <!-- no books message -->
+    <p>No books...</p>
+
+    <!-- add book form -->
+    <form>
+      <label>
+        Title:
+        <input type="text" name="title">
+      </label>
+      <label>
+        price:
+        <input type="number" name="price">
+      </label>
+      <button>Add book</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    books: [
+      {
+        title: 'The Catcher in the Rye',
+        price: 20
+      },
+      {
+        title: 'Of Mice and Men',
+        price: 18
+      }
+    ]
+  })
+}
+</script>
