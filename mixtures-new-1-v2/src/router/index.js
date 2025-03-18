237626a28396bd2c0e20e09ab03b60be792ac8c6
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-18 22:35:53 +0100

    Add routes for Color and Error pages

--- mixtures-new-1-v2/src/router/index.js
+++ mixtures-new-1-v2/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/HomePage.vue'
 import About from '@/pages/AboutPage.vue'
 import Mixin from '@/pages/MixinPage.vue'
+import ErrorPage from '@/pages/ErrorPage.vue'
+import ColorPage from '@/pages/ColorPage.vue'
 
 const routes = [
   {
@@ -18,6 +20,16 @@ const routes = [
     path: '/mixin',
     name: 'Mixin',
     component: Mixin
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'Error',
+    component: ErrorPage
+  },
+  {
+    path: '/color/:red?/:green?/:blue?',
+    name: 'Color',
+    component: ColorPage
   }
 ]
 
