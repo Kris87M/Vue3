6264d0b667807b1cc5a784e284bcaf7a88f55399
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-18 23:45:21 +0100

    Add SlideFadeAnimation component

---
+++ mixtures-new-1-v2/src/components/shared/SlideFadeAnimation.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition
+    name="custom-transition"
+    mode="out-in"
+    enter-active-class="animate__animated animate__fadeInLeft animate__faster"
+    leave-active-class="animate__animated animate__fadeOutLeft animate__faster">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'SlideFadeAnimation'
+}
+</script>
