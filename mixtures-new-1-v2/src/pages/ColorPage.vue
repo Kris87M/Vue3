974d0bc61093f210f809c9bf6b20b8f11d84c7f8
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-19 22:36:28 +0100

    Add alert display when color is not RGB

--- mixtures-new-1-v2/src/pages/ColorPage.vue
+++ mixtures-new-1-v2/src/pages/ColorPage.vue
@@ -2,23 +2,32 @@
   <div class="page">
     <PageTitle>
       Color
-      <p>You've picked a great color!</p>
     </PageTitle>
-    <flask-item
-    class="result-box__flask"
-      :size="15"
-      :color="linkColors"
-      :amount="100"
-      :buttons-visible="false" />
-    <div class="share__container">
-      <span class="pi pi-share-alt"></span>
-      <InputText
-        class="share__input"
-        type="text"
-        v-model="currentPath"
-        readonly
-        variant="filled"
-        :style="{ width: currentPath.length + 'ch' }"/>
+
+    <Message v-if="showAlert" severity="error">
+      This color is invalid! It's not RGB format...
+    </Message>
+
+    <div v-if="!showAlert">
+      <p>You've picked a great color!</p>
+      <flask-item
+        class="result-box__flask"
+        :size="15"
+        :color="linkColors"
+        :amount="100"
+        :buttons-visible="false"
+      />
+      <div class="share__container">
+        <span class="pi pi-share-alt" />
+        <InputText
+          class="share__input"
+          type="text"
+          v-model="currentPath"
+          readonly
+          variant="filled"
+          :style="{ width: currentPath.length + 'ch' }"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -27,13 +36,23 @@
 import PageTitle from '@/components/PageTitle.vue'
 import FlaskItem from '@/components/shared/FlaskItem.vue'
 import InputText from 'primevue/inputtext'
+import Message from 'primevue/message'
 
 export default {
   name: 'ColorPage',
+  data: () => ({
+    showAlert: false
+  }),
   beforeMount () {
     const { red, green, blue } = this.$route.params
     if (!red || !green || !blue) {
       this.$router.push('/')
+    } else if (
+      this.isInvalidColor(parseInt(red)) ||
+      this.isInvalidColor(parseInt(green)) ||
+      this.isInvalidColor(parseInt(blue))
+    ) {
+      this.showAlert = true
     }
   },
   computed: {
@@ -47,10 +66,17 @@ export default {
       return window.location.origin + this.$route.path
     }
   },
+  methods: {
+    isInvalidColor (value) {
+      console.log(value)
+      return isNaN(value) || value < 0 || value > 255
+    }
+  },
   components: {
     PageTitle,
     FlaskItem,
-    InputText
+    InputText,
+    Message
   }
 }
 </script>
