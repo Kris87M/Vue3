c860fc4a6892b2527ad58ce8e3cb82c839872f20
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-19 22:34:12 +0100

    Add Vue animated logo

--- mixtures-new-1-v2/src/pages/AboutPage.vue
+++ mixtures-new-1-v2/src/pages/AboutPage.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="page">
     <h1>About</h1>
+    <p>This app was built with Vue.js</p>
+    <img
+      src="@/assets/vue.png"
+      class="animate__animated"
+      :class="[isAnimated ? 'animate__jackInTheBox' : '']"
+      @animationend="isAnimated = false"
+      @click="jackInTheBox"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  data: () => ({
+    isAnimated: false
+  }),
+  mounted () {
+    this.isAnimated = true
+  },
+  methods: {
+    jackInTheBox () {
+      this.isAnimated = true
+    }
+  }
 }
 </script>
